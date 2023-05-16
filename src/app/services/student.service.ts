69423d3a33acb8421ab38cc655763522cc640c4e
crrkconfirm <97433447+crrkconfirm@users.noreply.github.com>
2023-05-16 12:32:04 -0700

    error handle implemented

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Student } from "../models/student";
-import { Observable } from 'rxjs';
+import { Observable, catchError, retry, throwError  } from 'rxjs';
   
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,21 @@ export class StudentService {
 
   public getStudents(): Observable<Student[]> 
   {
-    return this.httpClient.get<Student[]>(this.studentApiUrl);
+    return this.httpClient.get<Student[]>(this.studentApiUrl).pipe( retry(3), catchError(this.handleError));
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
 }
